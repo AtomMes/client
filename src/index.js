7f97c702b89a0c691dfea5e7bd8655663bb89bbe
AtomMes <atomik.2005@mail.ru>
2022-12-08 19:35:09 +0400

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import App from './App'
+import reducers from "./reducers";
 
+import App from "./App";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
